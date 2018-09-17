514052d44dcad11ec32da419e3d428b283b2b289
Karol Górski <karol.gorski@britenet.com.pl>
2018-09-17 05:55:16 +0200

    Enable fetching and retrieving data from firebase

--- src/app/recipes/store/recipe.effects.ts
+++ src/app/recipes/store/recipe.effects.ts
@@ -14,14 +14,13 @@ export class RecipeEffects {
   recipeFetch = this.actions$
     .ofType(RecipeActions.FETCH_RECIPES)
     .pipe(switchMap((action: RecipeActions.FetchRecipes) => {
-      return this.httpClient.get<Recipe[]>('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', {
+      return this.httpClient.get<Recipe[]>('https://ng-recipe-book-a41a5.firebaseio.com/recipes.json', {
         observe: 'body',
         responseType: 'json'
       });
     }), map(
       (recipes) => {
-        console.log(recipes);
-        for (let recipe of recipes) {
+        for (const recipe of recipes) {
           if (!recipe['ingredients']) {
             recipe['ingredients'] = [];
           }
@@ -38,7 +37,8 @@ export class RecipeEffects {
     .ofType(RecipeActions.STORE_RECIPES)
     .pipe(withLatestFrom(this.store.select('recipes')),
       switchMap(([action, state]) => {
-        const req = new HttpRequest('PUT', 'https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', state.recipes, {reportProgress: true});
+        const req = new HttpRequest('PUT',
+          'https://ng-recipe-book-a41a5.firebaseio.com/recipes.json', state.recipes, {reportProgress: true});
         return this.httpClient.request(req);
       }));
 
